b381acf42c9f40d48891b040a0b9b735f0339431
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-10-17 23:14:42 +0700

    refactor(models): rename `bluetooth_result.dart` to `bluetooth_device.dart`

--- lib/models/bluetooth_device.dart
+++ lib/models/bluetooth_device.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 
-/// {@template bluetooth_result}
+/// {@template bluetooth_device}
 /// Represents the result of a Bluetooth scan.
 /// {@endtemplate}
-class BluetoothResult extends Equatable {
-  /// {@macro bluetooth_result}
-  const BluetoothResult({this.name, this.address, this.rssi});
+class BluetoothDevice extends Equatable {
+  /// {@macro bluetooth_device}
+  const BluetoothDevice({this.name, this.address, this.rssi});
 
   /// The name of the Bluetooth device.
   final String? name;
@@ -16,9 +16,9 @@ class BluetoothResult extends Equatable {
   /// The signal strength of the Bluetooth device.
   final int? rssi;
 
-  /// Converts a [Map<String, dynamic>] to a [BluetoothResult].
-  factory BluetoothResult.fromJson(Map<String, dynamic> json) {
-    return BluetoothResult(
+  /// Converts a [Map<String, dynamic>] to a [BluetoothDevice].
+  factory BluetoothDevice.fromJson(Map<String, dynamic> json) {
+    return BluetoothDevice(
       name: json['name'],
       address: json['address'],
       rssi: json['rssi'],
