33364bb8db69f51d9205cb3a9d776ad16f1e947b
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-10-17 23:15:11 +0700

    refactor(models): rename `heart_rate_result.dart` to `heart_rate.dart`

--- lib/models/heart_rate.dart
+++ lib/models/heart_rate.dart
@@ -1,12 +1,12 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_veepoo_sdk/statuses/heart_statuses.dart';
 
-/// {@template heart_rate_result}
+/// {@template heart_rate}
 /// A class that represents the result of a heart rate detection.
 /// {@endtemplate}
-class HeartRateResult extends Equatable {
-  /// {@macro heart_rate_result}
-  const HeartRateResult(this.data, this.state);
+class HeartRate extends Equatable {
+  /// {@macro heart_rate}
+  const HeartRate(this.data, this.state);
 
   /// The heart rate data.
   final int data;
@@ -14,9 +14,9 @@ class HeartRateResult extends Equatable {
   /// The state of the heart.
   final HeartStatuses? state;
 
-  /// Converts a [Map<String, dynamic>] to a [HeartRateResult].
-  factory HeartRateResult.fromJson(Map<String, dynamic> map) {
-    return HeartRateResult(map['data'], HeartStatuses.fromString(map['state']));
+  /// Converts a [Map<String, dynamic>] to a [HeartRate].
+  factory HeartRate.fromJson(Map<String, dynamic> map) {
+    return HeartRate(map['data'], HeartStatuses.fromString(map['state']));
   }
 
   @override
